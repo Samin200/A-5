2efa92d117d4217e761d9a9eef5ba94fc8d50518
Samin200 <saminsafwan1@gmail.com>
2025-08-29 00:07:11 +0600

    call history and clear btn done

--- script.js
+++ script.js
@@ -4,16 +4,42 @@ const buttons = document.getElementsByClassName('call-btn')
 for(let i=0  ; i < buttons.length ; i++){
     buttons[i].addEventListener(
     'click' , function(){
-        coin = coin - 20
-        if(coin < 0){
+            coin -= 20  
+            coinBar.textContent = coin
+            const name = this.parentElement.parentElement.parentElement.children[0].children[1].textContent
+            const number = this.parentElement.parentElement.parentElement.children[1].children[0].textContent
+            
+            const callHistory = document.getElementById("call-history")
+            let history = document.createElement('div')
+            const div = document.createElement('div')
+            const div2 = document.createElement('div')
+            const div3 = document.createElement('div')
+            const div4 = document.createElement('div')
+            let now = new Date();
+            let currentTime = now.toLocaleTimeString('en-US', { hour:'2-digit', minute: '2-digit', second: '2-digit' })
+            history.classList.add("history" ,"flex","justify-between" ,"p-[16px]" ,"items-center" ,"rounded-2xl" ,"bg-[#FAFAFA]")
+            history.appendChild(div)
+            div.appendChild(div2)
+            div.appendChild(div3)
+            history.appendChild(div4)
+            div2.classList.add("font-bold")
+            div3.classList.add("text-gray-500")
+            div4.classList.add("text-gray-700")
+            div4.innerText = currentTime
+            div2.innerText = name
+            div3.innerText = number
+        if(coin < 20){
             alert("Cancel Call")
+            coin +=20
         }
         else{
-            coinBar.textContent = coin
-            const number = this.parentElement.parentElement.parentElement.children[0].children[1].textContent
-            const name = this.parentElement.parentElement.parentElement.children[1].children[0].textContent
-            alert(number + ' ' + name)
+            
+            alert(name + ' ' + number)
+            callHistory.append(history)
         }
+        document.getElementById('clear-btn').addEventListener("click", function(){
+            history.classList.add("hidden")
+        })
     }
 )
 }
@@ -24,6 +50,7 @@ for(let i=0  ; i < favBtn.length ; i++){
         favBtn[i].style.color = 'red'
         heart = heart + 1
         document.getElementById('heart-bar').textContent = heart
+        
     })
 }
 let copy = 0
@@ -38,3 +65,8 @@ for(let i=0  ; i < copyBtn.length ; i++){
     })
     
 }
+document.getElementById('clear-btn').addEventListener("click", function(){
+
+})
+
+
